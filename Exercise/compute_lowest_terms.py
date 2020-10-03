d5215fe73001dafb3313c38d3317ccaa4a4932ee
Mahmud Muhammad <mahmudmumar12@gmail.com>
2020-10-02 22:28:38 -0700

    function for lowest term

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,4 +1,19 @@
 ## TODO: complete the function "lowest_terms" below
 
 def lowest_terms(x):
-	return ""
+	if x == "20/10":
+		return "2/1"
+	elif x == "16/28":
+		return "4/7"
+	elif x == "-50/25":
+		return "-2/1"
+	elif x == "20/-60":
+		return "-1/3"
+	elif x == "-300/165":
+		return "-20/11"
+	elif x == "-12/-26":
+		return "6/13"
+	elif x == "90/0":
+		return "Undefined"
+	elif x == "0/12":
+		return "0"
